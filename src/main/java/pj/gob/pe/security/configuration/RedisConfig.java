3a8daba41f01c492e316d79facb2d3972425c638
Fernando Chavez <fernando.chavez@gdpteam.com>
2025-03-24 09:57:14 -0500

    feat: fixed redis config

--- src/main/java/pj/gob/pe/security/configuration/RedisConfig.java
+++ src/main/java/pj/gob/pe/security/configuration/RedisConfig.java
@@ -8,17 +8,12 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 @Getter
